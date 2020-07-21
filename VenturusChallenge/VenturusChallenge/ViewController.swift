d11bfa2f5b3f34442123e8e8772811d05a37a556
Ruyther Costa <ruyther.costa@foton.la>
2020-07-21 15:13:57 -0300

    Project Created

---
+++ VenturusChallenge/VenturusChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VenturusChallenge
+//
+//  Created by Foton on 21/07/20.
+//  Copyright © 2020 ruyther. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
